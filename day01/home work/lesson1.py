420fc11f9a301cdb37c81dd50f57f241bcc4bda9
gogaasatiani <gogaasatiani123@gmail.com>
2024-02-08 13:36:14 +0400

    second commit

---
+++ day01/home work/lesson1.py
@@ -0,0 +1,6 @@
+name = "my name is goga"
+surname = "my surname is asatiani"
+height = "i am 1.55 metres tall"
+age = "i am 12 years old"
+story = "I entered goal oriented academy for learn how to be a programmer"
+print (name + " " + surname + " " + height + " " + age + " " + story)
