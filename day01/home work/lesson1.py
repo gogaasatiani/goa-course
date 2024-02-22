9f86d9241a831475e6c3edc9d49e87086cb982c4
gogaasatiani <gogaasatiani123@gmail.com>
2024-02-22 22:34:42 +0400

    day02

--- day01/home work/lesson1.py
+++ day01/home work/lesson1.py
@@ -4,3 +4,9 @@ height = "i am 1.55 metres tall"
 age = "i am 12 years old"
 story = "I entered goal oriented academy for learn how to be a programmer"
 print (name + " " + surname + " " + height + " " + age + " " + story)
+
+
+
+
+
+
