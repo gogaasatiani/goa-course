8c8b75b6d351dd1a052a2489cd666ac6dcaa634f
gogaasatiani <gogaasatiani123@gmail.com>
2024-08-18 11:21:24 +0400

    all commit

---
+++ day49/classwork/index.py
@@ -0,0 +1,40 @@
+#classswork 1
+print(24 * 60 * 60)
+
+
+
+#classwork 2
+print("Price: 87$")
+
+#classwork 3
+print("Nickname: Master")
+print("Score: 99")
+
+#classwork 4
+
+print("   *   ")
+print("  ***  ")
+print(" ****** ")
+print("********")
+print(" ******  ")
+print("  ***    ")
+print("   *"     )
+
+#classwork 5
+country = input()
+capital = input()
+
+
+print("Country: " + country)
+print("Capital: " + capital)
+
+#classwork 6
+
+wins = input("Enter number of wins: ")
+ties = input("Enter number of ties: ")
+wins = int(wins)
+ties = float(ties)
+points = wins + (ties * 0.5)
+print("Points earned:", points)
+
+#classwork 7 
