a6ee8479ab98ceff5f2dc251c322ce58997aff2b
gogaasatiani <gogaasatiani123@gmail.com>
2024-10-06 13:06:15 +0400

    day66

---
+++ day66/classwork/script.js
@@ -0,0 +1,23 @@
+// 1. 
+let result1 = 8 % 3; // 2
+
+// 2. 
+let result2 = 14 % 5; // 4
+
+// 3. 
+let result3 = 27 % 4; // 3
+
+// 4. 
+let result4 = 30 % 6; // 0
+
+// 5. 
+let result5 = 11 % 2; // 1
+
+
+
+
+
+let balance = parseInt(readLine(), 10);
+let withdraw = parseInt(readLine(), 10);
+balance -= withdraw; 
+console.log(balance); 
---
+++ day66/homework/script.js
@@ -0,0 +1,2 @@
+// shevasworot kodi
+console.log('I\'m goga asatiani and studying  in\'goal oriented academi.');
