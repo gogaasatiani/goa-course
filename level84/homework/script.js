9876417fbbd35e290e3a1306f2d833df8aeaa302
gogaasatiani <gogaasatiani123@gmail.com>
2024-12-12 18:48:32 +0400

    level78, 82, 83, 84, 85

--- level84/homework/script.js
+++ level84/homework/script.js
@@ -16,3 +16,29 @@ if (isPrime(num)) {
 } else {
     console.log(num + " is not a prime number.");
 }
+
+
+function findDivisors(num) {
+    console.log(`Divisors of ${num}:`);
+    for (let i = 1; i <= num; i++) {
+        if (num % i === 0) {
+            console.log(i);
+        }
+    }
+}
+
+findDivisors(36);
+
+
+function sumOddNumbers() {
+    let sum = 0;
+
+    for (let i = 1; i <= 50; i++) {
+        if (i % 2 !== 0) { 
+            sum += i;
+        }
+    }
+
+    console.log(`The sum of all odd numbers between 1 and 50 is: ${sum}`);
+}
+sumOddNumbers();
--- level85/homework/script.js
+++ level85/homework/script.js
@@ -1,4 +1,36 @@
 
+let sum = 0;
+
+for (let i = 1; i <= 100; i++) {
+    sum += i;
+}
+console.log(`The sum of numbers from 1 to 100 is: ${sum}`);
+
+
+
+
+
+let num = 5;
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`${num} x ${i} = ${num * i}`);
+}
+
+
+let str = "javascript is fun";
+
+let char = 'a';
+
+let count = 0;
+
+for (let i = 0; i < str.length; i++) {
+    if (str[i] === char) {
+        count++;
+    }
+}
+console.log(`The character '${char}' appears ${count} times in the string.`);
+
+
 
 
 
