74fe8076be784847d3005307cee74e300999f49b
gogaasatiani <gogaasatiani123@gmail.com>
2025-01-10 21:12:39 +0400

    level 69

---
+++ group25 & 24/level 69/classwork/script.js
@@ -0,0 +1,10 @@
+
+// alert("my name is goga")
+// console.log("my name is goga")
+// alert("i am 13 years old")
+// console.log("i am 13 years old")
+// alert("my favourite car is BMW F60 V10")
+// console.log("my favourite car is BMW F60 V10")
+// alert("favourite color is black")
+// console.log("favourite color is black")
+
