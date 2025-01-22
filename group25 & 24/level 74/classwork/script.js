11101a5501e80ce85ff81aff2f73631a886bc996
gogaasatiani <gogaasatiani123@gmail.com>
2025-01-22 19:51:59 +0400

    individual project

---
+++ group25 & 24/level 74/classwork/script.js
@@ -0,0 +1,17 @@
+let Cofees = prompt('type that cofees number you like: ');
+
+switch (Cofees) {
+  case 0:
+    alert("Americano");
+    break;
+  case 1:
+    alert("Espresso");
+    break;
+  case 2:
+    alert("Cappuccino");
+    break;
+  default:
+    alert("არასწორი რიცხვი ჩაწერე მეგობარო");
+}
+
+
