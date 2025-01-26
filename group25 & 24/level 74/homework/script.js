4b6b2861dc984b2ae9ce1bee5a6d3802ab5ee64c
gogaasatiani <gogaasatiani123@gmail.com>
2025-01-26 12:16:57 +0400

    level 73, level 74

---
+++ group25 & 24/level 74/homework/script.js
@@ -0,0 +1,35 @@
+
+switch (weatherNumber) {
+    case 1:
+        return "მზიანი";
+    case 2:
+        return "წვიმიანი";
+    case 3:
+        return "მოღრუბლული";
+    case 4:
+        return "ქარიანი";
+    default:
+        return "არ არის დადგენილი";
+    }
+
+
+
+switch (dayNumber) {
+    case 1:
+        return "ორშაბათი";
+    case 2:
+        return "სამშაბათი";
+    case 3:
+        return "ოთხშაბათი";
+    case 4:
+        return "ხუთშაბათი";
+    case 5:
+        return "პარასკევი";
+    case 6:
+        return "შაბათი";
+    case 7:
+        return "კვირა";
+    default:
+          return "არასწორი დღე";
+      }
+        
