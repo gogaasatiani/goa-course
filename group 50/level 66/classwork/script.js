c5db9bb28b3cc3bcf7c91b58bbbe09d09a7d8ffd
gogaasatiani <gogaasatiani123@gmail.com>
2025-02-16 19:40:22 +0400

    level 65, level 66

---
+++ group 50/level 66/classwork/script.js
@@ -0,0 +1,15 @@
+console.log(10 && 20)
+console.log(10 || 6)
+console.log(true && false)
+console.log(15 && 5)
+console.log(10 || 5)
+
+
+
+
+
+let userName = prompt("enter your name: ")
+
+if (userName == 'goga') {
+    console.log("სეხნიები ვართ ჩემიძმა")
+}
